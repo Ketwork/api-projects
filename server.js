966b362a4143ccb5a1e1ae1494a3862d899a3906
Ket Patel <ketz555@hotmail.com>
2022-06-07 11:23:50 +0100

    alt short url method

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 const shortid = require('shortid');
+const { response } = require('express');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -44,7 +45,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-// Header Request
+// Request Header Parser Microservice
 app.get("/api/whoami", function (req, res) {
   res.json({
     // "values" : req.headers,
@@ -54,49 +55,102 @@ app.get("/api/whoami", function (req, res) {
   });
 });
 
+
+
+/* Create URL Model */
+let urlSchema = new mongoose.Schema({
+  original: { type: String, required: true },
+  short: { type: Number },
+});
+
+let Url = mongoose.model("Url", urlSchema);
+
+// let responseObject = {};
+app.post('/api/shorturl', bodyParser.urlencoded({ extended: false }) , (request, response) => {
+    let inputUrl = request.body['url']
+  
+    responseObject["original_url"] = inputUrl;
+
+    let inputShort = 1;
+  
+    //Find the highest short value and sets new short value one higher
+    Url.findOne({})
+      .sort({short: 'desc'})
+        .exec((error, result) => {
+          if(!error && result != undefined){
+            inputShort = result.short + 1
+          }
+          if (!error) {
+            Url.findOneAndUpdate(
+                // if original url exists it will be kept and short url is updated
+                {original: inputUrl},
+                {original: inputUrl, short: inputShort},
+                // if it doesn't exist it will be created (upsert: true)
+                {new: true, upsert: true },
+                (error, savedUrl)=> {
+                  if(!error){
+                    responseObject['short_url'] = savedUrl.short
+                    response.json(responseObject)
+                  }
+                }
+            )
+          }
+        
+      });
+  });
+
 // URL SHORTENING SERVICE
 
 // Build a schema and model to store saved URLS
-const ShortURL = mongoose.model('ShortUrl', mongoose.Schema({
-  short_url: String,
-  original_url: String,
-  suffix: String
-}));
-// create application/json parser
-app.use(bodyParser.urlencoded({ extended: "false" }));
-app.use(bodyParser.json());
-var jsonParser = bodyParser.json()
+// const ShortURL = mongoose.model('ShortUrl', mongoose.Schema({
+//   short_url: String,
+//   original_url: String,
+//   suffix: String
+// }));
+// // create application/json parser
+// app.use(bodyParser.urlencoded({ extended: "false" }));
+// app.use(bodyParser.json());
+// var jsonParser = bodyParser.json()
 
-app.post("/api/shorturl", function (req, res) {
-  let client_requested_url = req.body.url
-  let suffix = shortid.generate()
-  let newShortURL = suffix
+// app.post("/api/shorturl", function (req, res) {
+//   let client_requested_url = req.body.url
+//   let suffix = shortid.generate()
+//   let newShortURL = suffix
 
-  let newURL = new ShortURL({
-    short_url: __dirname + "/api/shorturl/" + suffix,
-    original_url: client_requested_url,
-    suffix: suffix
-  })
+//   let newURL = new ShortURL({
+//     short_url: __dirname + "/api/shorturl/" + suffix,
+//     original_url: client_requested_url,
+//     suffix: suffix
+//   })
 
-  newURL.save(function(err, doc) {
-    if (err) return console.error(err);
-    res.json({
-      "saved": true,
-      "short_url": newURL.short_url,
-      "original_URL": newURL.original_url,
-      "suffix": newURL.suffix
-    });
-  });
-});
+//   newURL.save(function(err, doc) {
+//     if (err) return console.error(err);
+//     res.json({
+//       "saved": true,
+//       "short_url": newURL.short_url,
+//       "original_URL": newURL.original_url,
+//       "suffix": newURL.suffix
+//     });
+//   });
+// });
 
 app.get("/api/shorturl/:suffix", function(req, res) {
   let userGeneratedSuffix = req.params.suffix;
-  ShortURL.find({suffix: userGeneratedSuffix}).then(function(foundUrls) {
-    let urlForRedirect = foundUrls[0].original_url
-    console.log(urlForRedirect, "<= url for redirect")
-    // express redirect
-    res.redirect(urlForRedirect)
-  });
+  // ShortURL.find({suffix: userGeneratedSuffix}).then(function(foundUrls) {
+  //   let urlForRedirect = foundUrls[0].original_url
+  //   console.log(urlForRedirect, "<= url for redirect")
+  //   // express redirect
+  //   res.redirect(urlForRedirect)
+  // });
+
+  ShortURL.findOne({suffix: userGeneratedSuffix}, (error, result) => {
+    if (!error && result != undefined){
+      console.log(result);
+      response.redirect(result.original_url)
+    } else {
+      response.json('URL not Found')
+    }
+  })
 });
 
 
